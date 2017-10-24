9f257023367fcc90741261441aaefebb7459f947
Juan Arias <juan.arias@marist.edu>
2017-10-23 23:46:58 -0400

    Feedback

--- labs/Lab6/TheAccountClass.java
+++ labs/Lab6/TheAccountClass.java
@@ -4,14 +4,16 @@ public class TheAccountClass {
 	public static void main(String[] args) {
 		Account account = new Account(1122, 20000);
 		
-		account.setAnnualInterestRate(5);
-		account.withdraw(45000);
-		account.deposit(100000);
+		account.setAnnualInterestRate(4.5); // JA
+		account.withdraw(2500);
+		account.deposit(3000);
 		
 		System.out.println("Account ID: " + account.getId());
 		System.out.println("Date Created: " + account.getDateCreated());
 		System.out.printf("Monthly interest: $%.2f\n ",
 				account.getMonthlyInterest());
+		System.out.println("Balance " +
+				account.getBalance());				
 	}
 
 }
